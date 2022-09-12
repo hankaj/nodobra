116a5398b0319284e2a6e4bd3498bf8cd6b0e1a7
hankaj <hanna.jarlaczynska@gmail.com>
2022-09-12 19:42:46 +0200

    Init repo with react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDrag } from 'react-dnd';
+// import { ItemTypes } from './Constants'
+
+function Node({ isDragging, text }) {
+  const [{ opacity }, dragRef] = useDrag(
+    () => ({
+      // type: ItemTypes.NODE,
+      item: { text },
+      collect: (monitor) => ({
+        opacity: monitor.isDragging() ? 0.5 : 1
+      })
+    }),
+    []
+  )
+  return (
+    <div ref={dragRef} style={{ opacity }}>
+      {text}
+    </div>
+  )
+}
 
 function App() {
   return (
