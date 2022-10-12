456c75979e758af4fd83a15f43c381479f5d22ff
micouy <m.powierza@tutanota.com>
2022-10-12 23:33:20 +0200

    Format code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,129 +1,153 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use tauri::{self, Manager};
-use std::{
-  sync::{Arc, Mutex},
-  path::PathBuf,
-  collections::HashMap,
-};
 use polars::prelude::*;
-use serde::{Serialize, Serializer, ser::SerializeSeq};
+use serde::{ser::SerializeSeq, Serialize, Serializer};
+use std::{
+    collections::HashMap,
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
+use tauri::{self, Manager};
 
 #[derive(Serialize, Debug)]
 #[serde(transparent)]
 struct State {
-  nodes: Vec<Node>,
+    nodes: Vec<Node>,
 }
 
 #[derive(Serialize, Debug)]
 #[serde(tag = "type", content = "data", rename_all = "kebab-case")]
 enum Node {
-  LoadData {
-    #[serde(rename = "columns", serialize_with = "serialize_columns")]
-    df: DataFrame
-  },
-  Multiply {},
-  Average {},
+    LoadData {
+        #[serde(rename = "columns", serialize_with = "serialize_columns")]
+        df: DataFrame,
+    },
+    Multiply {},
+    Average {},
 }
 
 use Node::*;
 
-fn serialize_columns<S>(df: &DataFrame, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-  let columns = df.get_column_names();
+fn serialize_columns<S>(df: &DataFrame, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let columns = df.get_column_names();
 
-  let mut seq = serializer.serialize_seq(Some(columns.len()))?;
+    let mut seq = serializer.serialize_seq(Some(columns.len()))?;
 
-  for column in columns {
-      seq.serialize_element(column)?;
-  }
+    for column in columns {
+        seq.serialize_element(column)?;
+    }
 
-  seq.end()
+    seq.end()
 }
 
 impl Node {
-  fn load_data<P>(file_path: P) -> Self where P: Into<PathBuf> {
-    let df = CsvReader::from_path(file_path)
-      .unwrap()
-      .has_header(true)
-      .finish()
-      .unwrap();
+    fn load_data<P>(file_path: P) -> Self
+    where
+        P: Into<PathBuf>,
+    {
+        let df = CsvReader::from_path(file_path)
+            .unwrap()
+            .has_header(true)
+            .finish()
+            .unwrap();
 
-    Node::LoadData { df }
-  }
+        Node::LoadData { df }
+    }
 
-  fn multiply() -> Self { Node::Multiply {} }
-  fn average() -> Self { Node::Average {} }
+    fn multiply() -> Self {
+        Node::Multiply {}
+    }
+    fn average() -> Self {
+        Node::Average {}
+    }
 }
 
 fn send_state(app: &tauri::AppHandle, state: &State) {
-  let mut result: Option<DataFrame> = None;
+    let mut result: Option<DataFrame> = None;
 
-  for node in state.nodes.iter() {
-    match node {
-      LoadData { df } => {
-        result = Some(df.clone());
-      },
-      Multiply {} => if let Some(ref mut df) = result {
-        df.replace_at_idx(0, (&df[0]) * 5.0);
-      },
-      Average {} => if let Some(ref mut df) = result {
-        *df = df.sum();
-      }
+    for node in state.nodes.iter() {
+        match node {
+            LoadData { df } => {
+                result = Some(df.clone());
+            }
+            Multiply {} => {
+                if let Some(ref mut df) = result {
+                    df.replace_at_idx(0, (&df[0]) * 5.0);
+                }
+            }
+            Average {} => {
+                if let Some(ref mut df) = result {
+                    *df = df.sum();
+                }
+            }
+        }
     }
-  }
 
-  #[derive(Serialize, Clone)]
-  struct Data<'a> {
-    nodes: &'a State,
-    #[serde(serialize_with = "df_to_string")]
-    result: &'a Option<DataFrame>,
-  }
+    #[derive(Serialize, Clone)]
+    struct Data<'a> {
+        nodes: &'a State,
+        #[serde(serialize_with = "df_to_string")]
+        result: &'a Option<DataFrame>,
+    }
 
-  let data = Data { nodes: state, result: &result };
+    let data = Data {
+        nodes: state,
+        result: &result,
+    };
 
-  app.emit_all("show-data", data).unwrap();
+    app.emit_all("show-data", data).unwrap();
 }
 
-fn df_to_string<S>(df: &Option<DataFrame>, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-  match df {
-    Some(df) => serializer.serialize_str(&df.to_string()),
-    None => serializer.serialize_none(),
-  }
+fn df_to_string<S>(df: &Option<DataFrame>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    match df {
+        Some(df) => serializer.serialize_str(&df.to_string()),
+        None => serializer.serialize_none(),
+    }
 }
 
 #[tauri::command]
 fn add_loader(state: tauri::State<Arc<Mutex<State>>>, app: tauri::AppHandle, file_path: String) {
-  let mut state = state.lock().unwrap();
-  state.nodes.push(Node::load_data(file_path));
+    let mut state = state.lock().unwrap();
+    state.nodes.push(Node::load_data(file_path));
 
-  send_state(&app, &state);
+    send_state(&app, &state);
 }
 
 #[tauri::command]
 fn add_multiplier(state: tauri::State<Arc<Mutex<State>>>, app: tauri::AppHandle) {
-  let mut state = state.lock().unwrap();
-  state.nodes.push(Node::multiply());
+    let mut state = state.lock().unwrap();
+    state.nodes.push(Node::multiply());
 
-  send_state(&app, &state);
+    send_state(&app, &state);
 }
 
 #[tauri::command]
 fn add_averager(state: tauri::State<Arc<Mutex<State>>>, app: tauri::AppHandle) {
-  let mut state = state.lock().unwrap();
-  state.nodes.push(Node::average());
+    let mut state = state.lock().unwrap();
+    state.nodes.push(Node::average());
 
-  send_state(&app, &state);
+    send_state(&app, &state);
 }
 
 fn main() {
-  let state = Arc::new(Mutex::new(State { nodes: vec![] }));
+    let state = Arc::new(Mutex::new(State { nodes: vec![] }));
 
-  tauri::Builder::default()
-    .manage(state)
-    .invoke_handler(tauri::generate_handler![add_loader, add_averager, add_multiplier])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(state)
+        .invoke_handler(tauri::generate_handler![
+            add_loader,
+            add_averager,
+            add_multiplier
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
